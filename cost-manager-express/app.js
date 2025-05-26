9c41fc786820e354502a5db7c7bd4f04734088e9
shoval <shov159@gmail.com>
2025-05-26 21:04:23 +0300

    more changes

--- cost-manager-express/app.js
+++ cost-manager-express/app.js
@@ -10,10 +10,21 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use("/api", apiRouter);
+app.get("/", (req, res) => {
+  res.send(`
+    <h1> Cost Manager API</h1>
+    <p>Status: <strong>Running</strong></p>
+    <p>Available endpoints:</p>
+    <ul>
+      <li><a href="/api/about">/api/about</a></li>
+      <li><a href="/api/users/123123">/api/users/:id</a></li>
+      <li><a href="/api/report?id=123123&year=2025&month=5">/api/report</a></li>
+    </ul>
+  `);
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
-
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.error(err));
 
